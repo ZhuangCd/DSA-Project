44ca06a428aeaff546563a9df5b332346c2c351b
ZhuangCd <126353820+ZhuangCd@users.noreply.github.com>
2025-03-25 18:02:02 +0100

    PQHeap.py upgraded to OOP

--- PQHeap.py
+++ PQHeap.py
@@ -3,66 +3,74 @@
 
 import sys
 
-def createEmptyPQ():
-    """Create and return an empty priority queue (min-heap)."""
-    return []
-
-def insert(A, e):
+class PriorityQueue:
     """
-    Insert element e into the priority queue A.
-    Maintains the min-heap property by percolating up.
+    A min-heap priority queue implementation using a Python list.
+    Supports insert and extractMin operations.
     """
-    A.append(e)
-    i = len(A) - 1 # Index of e
-    while i > 0 and A[parent(i)] > A[i]: # While parent is bigger
-        p = parent(i)
-        A[i], A[p] = A[p], A[i] # Switch parent and child
-        i = p # New index of e is i bc it moved up
 
-def extractMin(A):
-    """
-    Remove and return the smallest element from the priority queue A.
-    Maintains the min-heap property by percolating down.
-    """
-    if len(A) == 1:
-        return A.pop()
-    min_elem = A[0] # We take out the root as the smallest element
-    A[0] = A.pop() # We use the last element to become the root, O(1)
-    min_heapify(A, 0) # This takes O(log n)
-    return min_elem
-
-def min_heapify(A, i):
-    """
-    Ensure the subtree rooted at index i maintains the min-heap property.
-    This is a recursive downward operation.
-    """
-    left = 2 * i + 1
-    right = 2 * i + 2
-    smallest = i
-
-    if left < len(A) and A[left] < A[smallest]: # Check if Index exists and If left child smaller than parent 
-        smallest = left # Assign smallest to the value of a child
-    if right < len(A) and A[right] < A[smallest]:
-        smallest = right
-
-    if smallest != i: # Here if the smallest have been changed to child's index then:
-        A[i], A[smallest] = A[smallest], A[i] # Switch element and child
-        min_heapify(A, smallest) # Continue heapify downward
+    def __init__(self):
+        """Initialize an empty priority queue."""
+        self.heap = [] # List Attribute of the object self
 
 
-def parent(i):
-    """Return the index of the parent node for node at index i."""
-    return (i - 1) // 2
+    def insert(self, e):
+        """
+        Insert element e into the priority queue A.
+        Maintains the min-heap property by percolating up.
+        """
+        self.heap.append(e)
+        i = len(self.heap) - 1 # Index of e
+        while i > 0 and self.heap[self._parent(i)] > self.heap[i]: # While parent is bigger
+            p = self._parent(i)
+            self.heap[i], self.heap[p] = self.heap[p], self.heap[i] # Switch parent and child
+            i = p # New index of e is i bc it moved up
 
-# Just to test the code from terminal until we get the PQSort.py file
+    def extractMin(self):
+        """
+        Remove and return the smallest element from the priority queue A.
+        Maintains the min-heap property by percolating down.
+        """
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        min_elem = self.heap[0] # We take out the root as the smallest element
+        self.heap[0] = self.heap.pop() # We use the last element to become the root, O(1)
+        self._min_heapify(0) # This takes O(log n)
+        return min_elem
+
+    def _min_heapify(self, i):
+        """
+        Ensure the subtree rooted at index i maintains the min-heap property.
+        This is a recursive downward operation.
+        """
+        left = 2 * i + 1
+        right = 2 * i + 2
+        smallest = i
+
+        if left < len(self.heap) and self.heap[left] < self.heap[smallest]: # Check if Index exists and If left child smaller than parent 
+            smallest = left # Assign smallest to the value of a child
+        if right < len(self.heap) and self.heap[right] < self.heap[smallest]:
+            smallest = right
+
+        if smallest != i: # Here if the smallest have been changed to child's index then:
+            self.heap[i], self.heap[smallest] = self.heap[smallest], self.heap[i] # Switch element and child
+            self._min_heapify(smallest) # Continue heapify downward
+
+
+    def _parent(self, i):
+        """Return the index of the parent node for node at index i."""
+        return (i - 1) // 2
+    
+    def is_empty(self):
+        """Return True if the priority queue is empty."""
+        return len(self.heap) == 0
+
+
+
 if __name__ == "__main__":
-    pq = createEmptyPQ()
-    
-    for line in sys.stdin:
-        try:
-            insert(pq, int(line.strip()))
-        except:
-            pass
-    
-    while pq: #while its not empty
-        print(extractMin(pq))
+    pq = PriorityQueue()
+    for val in [34, 645, 3, -45, 1, 34, 0]:
+        pq.insert(val)
+
+    while not pq.is_empty():
+        print(pq.extractMin())
