c4ad50c15ef5ce2fb3d1aba04ad09d6551b483e1
ZhuangCd <126353820+ZhuangCd@users.noreply.github.com>
2025-05-09 17:40:20 +0200

    comments added

--- Project 3/Element.py
+++ Project 3/Element.py
@@ -4,11 +4,15 @@ from functools import total_ordering
 class Element:
 
     def __init__(self,key,data):
-        self.key = key #frequency
-        self.data = data #data (node)
+        self.key = key # frequency
+        self.data = data # Node object(char as integer 0-255)
 
     def __eq__(self,other):
         return self.key == other.key
 
     def __lt__(self,other):
         return self.key < other.key
+
+# like:
+# leaf_node = Node(97) 97 represents 'a'
+# element = Element(5, leaf_node) 5 is the frequency
--- Project 3/Node.py
+++ Project 3/Node.py
@@ -1,6 +1,6 @@
 class Node:
     def __init__(self, byte, left=None, right=None):
-        self.byte = byte       
+        self.byte = byte    # the char: 0-255  if its just an inner node without char: -1
         self.left = left
         self.right = right      
 
